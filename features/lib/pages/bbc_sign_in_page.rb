f7b034f6b2a58c08df15a7cbd5b142ea3e3233fa
manav <MPandey@spartaglobal.com>
2018-10-17 22:13:00 +0100

    added

---
+++ features/lib/pages/bbc_sign_in_page.rb
@@ -0,0 +1,4 @@
+
+class BBCSignInPage
+    include Capybara::DSL
+end
---
+++ features/step_definitions/bbc_login_step_defs.rb
@@ -0,0 +1,20 @@
+Given("I access the bbc login page") do
+    
+
+end
+
+And("I input incorrect username details") do
+pending # Write code here that turns the phrase above into concrete actions
+end
+
+And("I input incorrect password details") do
+pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I submit my details") do
+pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I receive and error for not finding the account") do
+pending # Write code here that turns the phrase above into concrete actions
+end
